b68f1e3111b8a089cc8b8ead08f1d77bbc39bb9f
RafaelOxy4 <rafael@grupoprimus.com.br>
2026-04-28 00:12:17 -0400

    Initial commit

---
+++ Integrasys.RouteManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Integrasys.RouteManager;
+
+public partial class AppShell : Shell
+{
+   public AppShell()
+   {
+      InitializeComponent();
+   }
+}
---
+++ Integrasys.RouteManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Integrasys.RouteManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+   protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
